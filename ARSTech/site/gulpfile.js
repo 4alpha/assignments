7d32b65c76c90cae957475cec6852ee120db410c
Aslam Sayyed <aslam@mutant-tech.com>
2017-02-27 12:50:26 +0530

    Changed to bootstrap version 3

--- ARSTech/site/gulpfile.js
+++ ARSTech/site/gulpfile.js
@@ -54,13 +54,10 @@ gulp.task('minify-js', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('lib/bootstrap'))
+        .pipe(gulp.dest('vendor/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('lib/jquery'))
-
-    gulp.src(['node_modules/tether/dist/js/tether.js', 'node_modules/tether/dist/js/tether.min.js'])
-        .pipe(gulp.dest('lib/tether'))
+        .pipe(gulp.dest('vendor/jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -70,7 +67,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('lib/font-awesome'))
+        .pipe(gulp.dest('vendor/font-awesome'))
 })
 
 // Run everything
