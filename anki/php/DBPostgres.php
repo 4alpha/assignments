9cadf70a7e921a37cf8bd71a9cdd3bc6b3fd2bed
Ankita B <ankita@4alpha.in>
2017-03-10 17:01:12 +0530

    Exceptions class created

--- anki/php/DBPostgres.php
+++ anki/php/DBPostgres.php
@@ -1,44 +1,51 @@
 <?php
 require 'interfaceDB.php';
 
+class IDAlreadyExist extends Exception {
+  public function getMessageForID() {
+  echo "ID Already exist Please enter another ID or Fill all values !!";
+  }
+}
+
+class ValueNotExist extends Exception {
+  public function valueNotExist() {
+    echo "ID does not exist or Fill all values !!";
+  }
+}
+
 class DBPostgres implements DB {
 
   public function __construct() {
-    $db_connect = pg_connect("host=localhost dbname=testdb user=postgres password=psql");
-    if(!$db_connect) {
-      echo "Unable to connect..";
-    } else {
-      echo "Database connected successfully..<br>";
-    }
-     
+    $this->db_connect = pg_connect("host=localhost dbname=testdb user=postgres password=psql") or die("unable to connect");
   }
   
   function insert($queryInsert) {
     try {
       $query = pg_query($queryInsert);
       if ($query == 0) {
-        throw new Exception('ID Already exist Please enter another ID or Fill all values !!');
+        throw new IDAlreadyExist;
       } else {
         $result = pg_fetch_object($query);
         return $result."<br>RECORD INSERTED SUCCESSFULLY...";
       }
-    } catch (Exception $e) {
-      echo $e->getMessage();
+    } catch (IDAlreadyExist $e) {
+      echo $e->getMessageForID();
     }
   }
+
   function update($queryUpdate) {
     try {
       $query = pg_query($queryUpdate);
       $queryVarUpdate = pg_affected_rows($query);
       if ($queryVarUpdate == 0 ) {
-         throw new Exception('Value does not exist or Fill all values !!');
+         throw new ValueNotExist;
       } else {
       $result = pg_fetch_row($query);
       return $result."<br>RECORD UPDATED SUCCESSFULLY...";
       }
     }
-    catch (Exception $e) {
-    echo $e->getMessage();
+    catch (ValueNotExist $e) {
+    echo $e->valueNotExist();
     }
   }
 
@@ -47,21 +54,21 @@ class DBPostgres implements DB {
       $query = pg_query($queryDelete);
       $queryVarDelete = pg_affected_rows($query);
       if($queryVarDelete == 0) {
-        throw new Exception('ID does not exist Please try again or Fill all values !!');
+        throw new ValueNotExist;
       } else {
           $result = pg_fetch_row($query);
           return $result."<br>RECORD DELETED SUCCESSFULLY...";
       }
-    } catch(Exception $e) {
-      echo $e->getMessage();
+    } catch(ValueNotExist $e) {
+      echo $e->valueNotExist();
     }
   }  
 
   function select($querySelectAll) {
     try {
-      $query = pg_query($querySelectAll);
+      $query = pg_query($this->db_connect,$querySelectAll);
       if ($query == false) {
-        throw new Exception('Record Not Found..:(');
+        throw new Exception(pg_last_error($this->db_connect));
       } else {
         while($result = pg_fetch_object($query)){
           print_r($result);
