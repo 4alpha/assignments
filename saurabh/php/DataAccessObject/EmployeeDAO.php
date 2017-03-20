3b616f2aa9191765a681680b6faba718384d69bb
Saurabh Ghadage <saurabh@4alpha.in>
2017-03-20 11:45:53 +0530

    updated files in MVC demo

--- saurabh/php/DataAccessObject/EmployeeDAO.php
+++ saurabh/php/DataAccessObject/EmployeeDAO.php
@@ -2,33 +2,56 @@
   namespace DataAccessObject;
   use DatabaseFiles\Database as Database;
   use ConfigurationFile\Configuration as Configuration;
+  use AppExceptions\GetAllRecordException as GetAllRecordException;
+  use AppExceptions\InsertRecordException as InsertRecordException;
+  use AppExceptions\UpdateRecordException as UpdateRecordException;
+  use AppExceptions\DeleteRecordException as DeleteRecordException;
+
+
 
   class EmployeeDAO implements DAO {
-    public $dbpostgres;
+    private $dbpostgres;
     
     function __construct() {    
       $this->dbpostgres = Database::getDatabaseConnection();
     }
     
-    function getAll() {
-      $query = 'SELECT * FROM employees;';
-      return $this->dbpostgres->select($query);
+    public function getAll() {
+      try {
+        $query = 'SELECT * FROM employees;';
+        $result =  $this->dbpostgres->select($query);
+        $getArray = pg_fetch_array($result);
+        return $getArray;
+      } catch (GetAllRecordException $e) {
+        return $e->getErrorMessage();
+      }
     }
     
-    function insert($employee) {
-      $query = "INSERT INTO employees VALUES('".$employee->emp_no."','".$employee->firstName."','".$employee->lastName."','".$employee->hireDate."');";
-      return $this->dbpostgres->insert($query);
-
+    public function insert($employee) {
+      try {
+        $query = "INSERT INTO employees VALUES('" . $employee->emp_no . "', '" . $employee->firstName . "', '" . $employee->lastName . "', '" . $employee->hireDate . "');";
+        return $this->dbpostgres->insert($query);
+      } catch(InsertRecordException $e) {
+        return $e->getErrorMessage();
+      }
     }
     
-    function update($employee) {
-      $query = "UPDATE employees SET first_name='".$employee->firstName."', last_name='".$employee->lastName."', hire_date='".$employee->hireDate."' where emp_no = '".$employee->emp_no."';";
-      return $this->dbpostgres->update($query);
+    public function update($employee) {
+      try {
+        $query = "UPDATE employees SET first_name = '" . $employee->firstName . "', last_name = '" . $employee->lastName . "', hire_date = '" . $employee->hireDate . "' where emp_no = '" . $employee->emp_no . "';";
+        return $this->dbpostgres->update($query);
+      } catch (UpdateRecordException $e) {
+        return $e->getErrorMessage();
+      }
     }
     
-    function delete($emp_no) {
-      $query = "DELETE FROM employees WHERE emp_no = '".$emp_no."';";
-      return $this->dbpostgres->delete($query);
+    public function delete($emp_no) {
+      try {
+        $query = "DELETE FROM employees WHERE emp_no = '" . $emp_no . "' ;";
+        return $this->dbpostgres->delete($query);
+      } catch (DeleteRecordException $e) {
+          return $e->getErrorMessage($this->dbconnection);
+      }
     }
     
   }
