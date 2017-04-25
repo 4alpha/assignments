055b4e8aa9389081bde08d2919037b229dc4109b
Karishma W <karisham@4alpha.in>
2017-04-25 18:13:31 +0530

    unit testing updated

---
+++ karishma/php/NewView/Jasmine/src/Calculator.js
@@ -0,0 +1,14 @@
+Calculator = function() {};
+
+Calculator.prototype.addition = function(number1, number2){
+  return number1 + number2;
+}
+Calculator.prototype.subtraction = function(number1, number2){
+  return number1 - number2;
+}
+Calculator.prototype.multiplication = function(number1, number2){
+  return number1 * number2;
+}
+Calculator.prototype.division = function(number1, number2){
+  return number1 / number2;
+}
