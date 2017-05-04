47d266a74fd694d839071056ec45392c35aef6a5
Aslam Sayyed <aslam@mutant-tech.com>
2017-05-04 15:46:13 +0530

    Sample test

--- aslam/tdd/js/app.js
+++ aslam/tdd/js/app.js
@@ -1,3 +1,24 @@
-function sum(a, b) {
-  return a + b;
-}
+'use strict'
+var leaaApp = angular.module("LeAaApp", ['restangular']);
+leaaApp.config(function(RestangularProvider) {
+  RestangularProvider.setBaseUrl('/rest');
+})
+
+leaaApp.controller("UserCtrl", function($scope, Restangular) {
+  var ctrl = this;
+  ctrl.users = null;
+
+  ctrl.getUsers = function() {
+    var userSvc = Restangular.all('admin/users');
+    userSvc.getList().then(function(data) {
+      ctrl.users = data;
+    }, function(error) {
+      ctrl.users = [];
+    });
+  }
+
+  function init() {
+    ctrl.getUsers();
+  }
+  init();
+});
